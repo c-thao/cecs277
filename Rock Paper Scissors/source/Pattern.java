9cf6bf2fbb6caba90e31c8d90d674012fa4504ba
Chou Thao <44004960+c-thao@users.noreply.github.com>
2018-10-28 01:04:13 -0700

    Update Pattern.java
    Updated the javadocs for the Pattern file.

--- Rock Paper Scissors/source/Pattern.java
+++ Rock Paper Scissors/source/Pattern.java
@@ -1,23 +1,61 @@
 import java.io.Serializable;
 
+/**
+ * This is a Pattern Class which contains a local String
+ * cohering to a series of varying characters and/or
+ * integers correlating to a pattern.
+ * 
+ * @author Chou Thao
+ *
+ */
 public class Pattern implements Serializable {
+	/** A local String for a Pattern Object */
+	private String decisions;
 	
-	String decisions;
-	
+	/**
+	 * A constructor of a Pattern object
+	 * which initializes a local String.
+	 * 
+	 * @param choice initial value for
+	 *               local String decisions
+	 */
 	public Pattern (String choice) {
 		decisions = choice;
 	}
 	
+	/**
+	 * Gets local String decisions.
+	 * 
+	 * @return the local String
+	 */
 	public String getPattern() {
 		return decisions;
 	}
 	
+	/**
+	 * Gets the hash code for a
+	 * Pattern object base upon
+	 * its local String's hash
+	 * code.
+	 * 
+	 */
 	public int hashCode() {
 		return decisions.hashCode();
 	}
 	
-	
-	
+	/**
+	 * Checks to see if an Object is
+	 * of the data type Pattern as
+	 * well as being the same Pattern
+	 * comparing their local String
+	 * returning true, else return false.
+	 * 
+	 * @param data the object to compare
+	 * @return     true if the object is
+	 *             equal to this Pattern
+	 *             object
+	 * 
+	 */
 	public boolean equals(Object data) {
 		if (this == data) {
 			return true;
