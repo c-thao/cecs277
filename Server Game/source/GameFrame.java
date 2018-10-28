b04fb69ce06dee8073a26e7fe78b0b276669a877
Chou Thao <44004960+c-thao@users.noreply.github.com>
2018-10-28 01:06:36 -0700

    Update GameFrame.java
    Updated the javadocs to expand on the details for this class.

--- Server Game/source/GameFrame.java
+++ Server Game/source/GameFrame.java
@@ -2,16 +2,26 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 
+/**
+ * This is a GameFrame class which is a GUI
+ * design for the game.
+ * 
+ * @author Chou Thao
+ *
+ */
 public class GameFrame extends JFrame {
-	// choices
+	/** The userChoice. */
 	private String userChoice;
 
-	// panels
+	/** The panels of the GUI. */
 	private JPanel panel1, panel2, panel3;
 
-	// label
+	/** The labels of the GUI. */
 	private JLabel label1, label2, label3, label4, label5, label6, label7, label8;
-
+	
+	/**
+	 * Default Constructor.
+	 */
 	public GameFrame() {
 		userChoice = "";
 
@@ -37,48 +47,86 @@ public class GameFrame extends JFrame {
 		// sets size of frame
 		setBounds(100, 400, 500, 700);
 	}
-
+	
+	/**
+	 * Gets the userChoice.
+	 * 
+	 * @return the String userChoice
+	 */
 	public String getUserChoice() {
 		return userChoice;
 	}
 	
+	/**
+	 * Sets the userChoice to an empty String.
+	 */
 	public void resetUserChoice() {
 		userChoice = "";
 	}
-
+	
+	/**
+	 * Sets the text of label1 with the string for
+	 * userChoice
+	 */
 	public void updateLabel1() {
 		label1.setText("Player throws " + userChoice);
 	}
-
+	
+	/**
+	 * Sets the text of label2 with the string
+	 * input compChoice.
+	 * 
+	 * @param compChoice the String input to update
+	 *                   the text of label2
+	 */
 	public void updateLabel2(String compChoice) {
 		label2.setText("Computer throws " + compChoice);
 	}
-
+	
+	/**
+	 * Sets the text of label3 with the tally of the
+	 * game with integer inputs, w, l, and t.
+	 * 
+	 * @param w the integer input for wins
+	 * @param l the integer input for losses
+	 * @param t the integer input for ties
+	 */
 	public void updateLabel3(int w, int l, int t) {
 		label3.setText("Current Score: T " + t +
 				" W " + w + " L " + l);
 		}
 
+	/**
+	 * Sets the text of label4 with an String.
+	 * 
+	 * @param output the String to update the
+	 *               text of label4
+	 */
 	public void updateLabel4(String output) {
 		label4.setText(output);
 	}
-
+	
+	/**
+	 * Builds the components of the GUI adding
+	 * action listeners to handle button inter-
+	 * actions.
+	 */
 	public void createComponents() {
-		// buttons
+		// initializes all the buttons
 		JButton rock = new JButton(
-				new ImageIcon(new ImageIcon("rock.jpg").getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT)));
+				new ImageIcon(new ImageIcon("src/rock.jpg").getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT)));
 		rock.setPreferredSize(new Dimension(100, 100));
 		JButton paper = new JButton(new ImageIcon(
-				new ImageIcon("\\paper.jpg").getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT)));
+				new ImageIcon("src/paper.jpg").getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT)));
 		paper.setPreferredSize(new Dimension(100, 100));
 		JButton scissors = new JButton(new ImageIcon(
-				new ImageIcon("\\scissors.png").getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT)));
+				new ImageIcon("src/scissors.png").getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT)));
 		scissors.setPreferredSize(new Dimension(100, 100));
 		JButton quit = new JButton(new ImageIcon(
-				new ImageIcon("\\quit.jpg").getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT)));
+				new ImageIcon("src/quit.jpg").getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT)));
 		quit.setPreferredSize(new Dimension(100, 100));
 
-		// putting components onto panel then frame
+		// put all components onto the panel then frame
 		panel1.add(label5);
 		panel1.add(rock);
 		panel1.add(label6);
@@ -93,7 +141,7 @@ public class GameFrame extends JFrame {
 		add(label3, BorderLayout.NORTH);
 		add(label4, BorderLayout.SOUTH);
 
-		// button events
+		// add listeners to the buttons
 		rock.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent event) {
 				userChoice = "R";
