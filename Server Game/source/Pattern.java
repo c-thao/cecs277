49d20edf83fc4cfda23b47c35748411943bab771
Chou Thao <44004960+c-thao@users.noreply.github.com>
2018-10-28 01:07:09 -0700

    Update Pattern.java
    Updated and tidied up the javadocs for this class.

--- Server Game/source/Pattern.java
+++ Server Game/source/Pattern.java
@@ -1,38 +1,30 @@
 import java.io.Serializable;
 
 /**
- * this is a Pattern Object which
- * contains a local String cohering
- * to a series of varying characters
- * and/or integers correlating to a
- * pattern
+ * This is a Pattern Class which contains a local String
+ * cohering to a series of varying characters and/or
+ * integers correlating to a pattern.
  * 
  * @author Chou Thao
  *
  */
 public class Pattern implements Serializable {
-	/**
-	 * a local String for
-	 * a Pattern Object
-	 * 
-	 */
+	/** A local String for a Pattern Object */
 	private String decisions;
 	
 	/**
-	 * a constructor of a Pattern
-	 * object which initializes 
-	 * a local String
+	 * A constructor of a Pattern object
+	 * which initializes a local String.
 	 * 
-	 * @param choice initial value
-	 * 				 for local String
-	 * 				 decisions
+	 * @param choice initial value for
+	 *               local String decisions
 	 */
 	public Pattern (String choice) {
 		decisions = choice;
 	}
 	
 	/**
-	 * gets local String decisions
+	 * Gets local String decisions.
 	 * 
 	 * @return the local String
 	 */
@@ -41,10 +33,10 @@ public class Pattern implements Serializable {
 	}
 	
 	/**
-	 * gets the hash code for
-	 * a Pattern object base
-	 * upon its local String's
-	 * hash code
+	 * Gets the hash code for a
+	 * Pattern object base upon
+	 * its local String's hash
+	 * code.
 	 * 
 	 */
 	public int hashCode() {
@@ -52,11 +44,16 @@ public class Pattern implements Serializable {
 	}
 	
 	/**
-	 * checks to see if an Object is
+	 * Checks to see if an Object is
 	 * of the data type Pattern as
 	 * well as being the same Pattern
 	 * comparing their local String
-	 * returning true else return false
+	 * returning true, else return false.
+	 * 
+	 * @param data the object to compare
+	 * @return     true if the object is
+	 *             equal to this Pattern
+	 *             object
 	 * 
 	 */
 	public boolean equals(Object data) {
